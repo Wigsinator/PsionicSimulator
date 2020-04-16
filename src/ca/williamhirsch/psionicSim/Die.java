f6277fb64d49d2113d1461c58f7dc3102756d499
William Hirsch <wigsinator@gmail.com>
2020-04-16 14:14:45 -0400

    Restructure Die as abstract class instead of Interface

--- src/ca/williamhirsch/psionicSim/Die.java
+++ src/ca/williamhirsch/psionicSim/Die.java
@@ -1,7 +1,28 @@
 package ca.williamhirsch.psionicSim;
 
-public interface Die {
-	int roll();
-	Die increase();
-	Die decrease();
+import java.util.Random;
+
+abstract class Die {
+	private int size;
+	private Random randomizer;
+	
+	public Die() {
+		randomizer = new Random();
+	}
+	
+	int roll() {
+		return randomizer.nextInt(size)+1;
+	}
+	
+	Die increase() {
+		return null;
+	}
+	
+	Die decrease() {
+		return null;
+	}
+	
+	int getSize() {
+		return size;
+	}
 }
